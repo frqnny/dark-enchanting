87c15835aa5f9ca8e4c5023c3d19fd3baf715c6f
Hazel Fidecaro <hazelfidecaro@gmail.com>
2021-09-05 15:20:37 +1200

    Changed enchanting table to check for the `c:bookshelves` tag instead of `mincraft:bookshelf` block.

--- src/main/java/io/github/frqnny/darkenchanting/util/BookcaseUtils.java
+++ src/main/java/io/github/frqnny/darkenchanting/util/BookcaseUtils.java
@@ -1,39 +1,44 @@
 package io.github.frqnny.darkenchanting.util;
 
-import net.minecraft.block.Blocks;
+import net.minecraft.block.Block;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
 public class BookcaseUtils {
     public static int getBookshelfCount(World world, BlockPos blockPos) {
         int bookshelves = 0;
 
+        Tag<Block> bookshelvesTag = world.getTagManager().getTag(Registry.BLOCK_KEY, new Identifier("c", "bookshelves"), id -> new RuntimeException("Could not load tag: " + id.toString()));
+
         int j;
         for (j = -1; j <= 1; ++j) {
             for (int k = -1; k <= 1; ++k) {
                 if ((j != 0 || k != 0) && world.isAir(blockPos.add(k, 0, j)) && world.isAir(blockPos.add(k, 1, j))) {
-                    if (world.getBlockState(blockPos.add(k * 2, 0, j * 2)).isOf(Blocks.BOOKSHELF)) {
+                    if (world.getBlockState(blockPos.add(k * 2, 0, j * 2)).isIn(bookshelvesTag)) {
                         ++bookshelves;
                     }
 
-                    if (world.getBlockState(blockPos.add(k * 2, 1, j * 2)).isOf(Blocks.BOOKSHELF)) {
+                    if (world.getBlockState(blockPos.add(k * 2, 1, j * 2)).isIn(bookshelvesTag)) {
                         ++bookshelves;
                     }
 
                     if (k != 0 && j != 0) {
-                        if (world.getBlockState(blockPos.add(k * 2, 0, j)).isOf(Blocks.BOOKSHELF)) {
+                        if (world.getBlockState(blockPos.add(k * 2, 0, j)).isIn(bookshelvesTag)) {
                             ++bookshelves;
                         }
 
-                        if (world.getBlockState(blockPos.add(k * 2, 1, j)).isOf(Blocks.BOOKSHELF)) {
+                        if (world.getBlockState(blockPos.add(k * 2, 1, j)).isIn(bookshelvesTag)) {
                             ++bookshelves;
                         }
 
-                        if (world.getBlockState(blockPos.add(k, 0, j * 2)).isOf(Blocks.BOOKSHELF)) {
+                        if (world.getBlockState(blockPos.add(k, 0, j * 2)).isIn(bookshelvesTag)) {
                             ++bookshelves;
                         }
 
-                        if (world.getBlockState(blockPos.add(k, 1, j * 2)).isOf(Blocks.BOOKSHELF)) {
+                        if (world.getBlockState(blockPos.add(k, 1, j * 2)).isIn(bookshelvesTag)) {
                             ++bookshelves;
                         }
                     }
