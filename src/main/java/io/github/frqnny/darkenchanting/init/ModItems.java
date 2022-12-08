13a2b9ee103c36a6dc0c5e112c2633bdf74b0d0d
ayutac <fly.high.android@gmail.com>
2022-12-08 15:56:40 +0100

    new creative menu and torch block

--- src/main/java/io/github/frqnny/darkenchanting/init/ModItems.java
+++ src/main/java/io/github/frqnny/darkenchanting/init/ModItems.java
@@ -5,21 +5,45 @@ import io.github.frqnny.darkenchanting.block.DarkConduitBlock;
 import io.github.frqnny.darkenchanting.block.DarkEnchanterBlock;
 import io.github.frqnny.darkenchanting.item.TableUpgradeItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.WallStandingBlockItem;
+import net.minecraft.item.Items;
+import net.minecraft.item.VerticallyAttachableBlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Rarity;
 import net.minecraft.registry.Registry;
+import net.minecraft.util.math.Direction;
 
 public class ModItems {
-    public static final Item DARK_ENCHANTER = new BlockItem(ModBlocks.DARK_ENCHANTER, new FabricItemSettings().group(DarkEnchanting.DARK_ENCHANTING));
-    public static final Item DARK_TORCH = new WallStandingBlockItem(ModBlocks.DARK_TORCH, ModBlocks.DARK_TORCH_WALL, new FabricItemSettings().group(DarkEnchanting.DARK_ENCHANTING));
-    public static final Item TABLE_UPGRADE = new TableUpgradeItem(new FabricItemSettings().group(DarkEnchanting.DARK_ENCHANTING).maxCount(1).rarity(Rarity.EPIC));
+
+    public static final Item DARK_ENCHANTER = new BlockItem(ModBlocks.DARK_ENCHANTER, new FabricItemSettings());
+    public static final Item DARK_TORCH = new VerticallyAttachableBlockItem(ModBlocks.DARK_TORCH, ModBlocks.DARK_TORCH_WALL, new FabricItemSettings(), Direction.DOWN);
+    public static final Item TABLE_UPGRADE = new TableUpgradeItem(new FabricItemSettings().maxCount(1).rarity(Rarity.EPIC));
 
     public static void init() {
         Registry.register(Registries.ITEM, DarkEnchanterBlock.ID, DARK_ENCHANTER);
         Registry.register(Registries.ITEM, DarkConduitBlock.ID, DARK_TORCH);
         Registry.register(Registries.ITEM, TableUpgradeItem.ID, TABLE_UPGRADE);
+        ItemGroupEvents.modifyEntriesEvent(DarkEnchanting.DARK_ENCHANTING).register(ModItems::addDarkEnchantingBlocks);
+        ItemGroupEvents.modifyEntriesEvent(DarkEnchanting.DARK_ENCHANTING).register(ModItems::addFunctionalBlocks);
+        ItemGroupEvents.modifyEntriesEvent(DarkEnchanting.DARK_ENCHANTING).register(ModItems::addTools);
     }
+
+    private static void addDarkEnchantingBlocks(FabricItemGroupEntries entries) {
+        entries.add(DARK_ENCHANTER);
+        entries.add(DARK_TORCH);
+        entries.add(TABLE_UPGRADE);
+    }
+
+    private static void addFunctionalBlocks(FabricItemGroupEntries entries) {
+        entries.addAfter(Items.SOUL_TORCH, DARK_TORCH);
+        entries.addAfter(Items.ENCHANTING_TABLE, DARK_ENCHANTER);
+    }
+
+    private static void addTools(FabricItemGroupEntries entries) {
+        entries.addBefore(Items.ENDER_EYE, TABLE_UPGRADE);
+    }
+
 }
