8fd0f084390fbc5c67b69517afc6e323c52c4294
globalista-dev <source.globalista@gmail.com>
2023-08-02 13:25:05 -0300

    Items now show in-game. They are properly registered in the mod's item group. Bump to version 1.0.4 because of the bugfix.

--- src/main/java/io/github/frqnny/darkenchanting/DarkEnchanting.java
+++ src/main/java/io/github/frqnny/darkenchanting/DarkEnchanting.java
@@ -4,7 +4,20 @@ import draylar.omegaconfig.OmegaConfig;
 import io.github.frqnny.darkenchanting.config.DarkEnchantingConfig;
 import io.github.frqnny.darkenchanting.init.*;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+
+import static io.github.frqnny.darkenchanting.init.ModBlocks.DARK_TORCH;
+import static io.github.frqnny.darkenchanting.init.ModItems.DARK_ENCHANTER;
+import static io.github.frqnny.darkenchanting.init.ModItems.TABLE_UPGRADE;
 
 public class DarkEnchanting implements ModInitializer {
     public static final String MODID = "dark-enchanting";
@@ -16,6 +29,8 @@ public class DarkEnchanting implements ModInitializer {
         return new Identifier(MODID, namespace);
     }
 
+    public static final RegistryKey<ItemGroup> DarkEnchantingGroup = RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier("dark-enchanting", "dark_enchanting_group"));
+
     @Override
     public void onInitialize() {
         ModItems.init();
@@ -23,5 +38,17 @@ public class DarkEnchanting implements ModInitializer {
         ModGUIs.init();
         ModTags.init();
         ModPackets.init();
+
+        Registry.register(Registries.ITEM_GROUP, DarkEnchantingGroup, FabricItemGroup.builder()
+                .icon(() -> new ItemStack(DARK_ENCHANTER))
+                .displayName(Text.translatable("itemGroup.dark-enchanting.dark_enchanting_group"))
+                .build());
+
+        ItemGroupEvents.modifyEntriesEvent(DarkEnchantingGroup).register(content -> {
+            content.add(DARK_ENCHANTER);
+            content.add(DARK_TORCH);
+            content.add(TABLE_UPGRADE);
+        });
+
     }
 }
